4caa39a1361c02033c9c35d4ae8735b1f318768d
Noahsa-HiNg <102230149@sv1.dut.udn.vn>
2025-05-12 21:34:05 +0700

    nothing

---
+++ EnglishStudySystem/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EnglishStudySystem.Models;
+
+namespace EnglishStudySystem.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        private readonly ApplicationDbContext _context = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public ActionResult ShowAllCategories()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
