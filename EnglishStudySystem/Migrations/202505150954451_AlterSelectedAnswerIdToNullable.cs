51ef8db435ac40e7d0cca4ff97cee09e01bec4b9
Anh Lê Hữu Tuấn <102230142@sv1.dut.udn.vn>
2025-05-15 19:17:15 +0700

    full bài kiểm tra

---
+++ EnglishStudySystem/Migrations/202505150954451_AlterSelectedAnswerIdToNullable.cs
@@ -0,0 +1,20 @@
+﻿namespace EnglishStudySystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AlterSelectedAnswerIdToNullable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.UserAnswers", "SelectedAnswerId", c => c.Int()); // Perment null
+                                                                              // Có thể có các thay đổi khác nếu bạn có những thay đổi model khác
+        }
+
+        public override void Down()
+        {
+            AlterColumn("dbo.UserAnswers", "SelectedAnswerId", c => c.Int(nullable: false)); // Revert to not null
+                                                                                             // Có thể có các thay đổi khác
+        }
+    }
+}
