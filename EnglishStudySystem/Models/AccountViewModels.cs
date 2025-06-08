cf97b94e26ffc474ed8f872ce7751d2cd9cd91f9
Anh Lê Hữu Tuấn <102230142@sv1.dut.udn.vn>
2025-06-08 22:29:06 +0700

    update

--- EnglishStudySystem/Models/AccountViewModels.cs
+++ EnglishStudySystem/Models/AccountViewModels.cs
@@ -120,15 +120,17 @@ namespace EnglishStudySystem.Models
         [Display(Name = "Email")]
         public string Email { get; set; }
 
-        [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [StringLength(100, ErrorMessage = "{0} phải có ít nhất {2} ký tự.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Mật khẩu")]
+        // Quy tắc: Ít nhất 1 ký tự đặc biệt, ít nhất 1 số, và ít nhất 1 chữ cái viết hoa và 1 chữ cái viết thường
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{6,}$", ErrorMessage = "Mật khẩu phải có ít nhất 6 ký tự, bao gồm chữ hoa, chữ thường, số và ký tự đặc biệt.")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Display(Name = "Xác nhận mật khẩu")]
+        [Compare("Password", ErrorMessage = "Mật khẩu và mật khẩu xác nhận không khớp.")]
         public string ConfirmPassword { get; set; }
 
         public string Code { get; set; }
