adb9cdf6d22b3a495b1c47cd2208ff07ccd93ad0
Anh Lê Hữu Tuấn <102230142@sv1.dut.udn.vn>
2025-06-09 22:46:56 +0700

    update test

--- EnglishStudySystem/Controllers/TestController.cs
+++ EnglishStudySystem/Controllers/TestController.cs
@@ -86,7 +86,7 @@ namespace EnglishStudySystem.Controllers
             {
                 return RedirectToAction("Details", "Lesson",new { id = attempt.Test.LessonId });
             }
-            int time = attempt.Test.QuestionCount * 1 + 5;
+            int time = attempt.Test.Duration;
             // Tính thời gian còn lại
             var timeRemaining = TimeSpan.FromMinutes(time) - (DateTime.Now - attempt.StartTime);
             ViewBag.TimeRemaining = timeRemaining.TotalSeconds > 0 ? timeRemaining : TimeSpan.Zero;
